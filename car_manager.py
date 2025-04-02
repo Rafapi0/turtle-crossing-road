4f7a84a83df00add55c455ab490a173252a455ef
Rafapi0 <rafaelpioprofissional@gmail.com>
2025-04-02 05:57:19 +0100

    car_manager modificado modificações adicionadas ao main.py

--- car_manager.py
+++ car_manager.py
@@ -8,7 +8,7 @@ MOVE_INCREMENT = 10
 
 class CarManager:
     def __init__(self):
-        all_cars = []
+        self.all_cars = []
     
     def create_car(self):
         new_car = Turtle("square")
@@ -18,3 +18,7 @@ class CarManager:
         random_y = random.randint(-240, 240)
         new_car.goto(300, random_y)
         self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
